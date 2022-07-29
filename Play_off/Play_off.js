26eebd85625039a6dc19fb64c1e77445070fa33c
Kevin Romero Villa <kromerov1@outlook.com>
2022-07-28 19:14:16 -0500

    Terminada la parte de cuartos de finales

--- Play_off/Play_off.js
+++ Play_off/Play_off.js
@@ -18,4 +18,71 @@ export default class Play_off {
         }
     }
 
+    generateGoals(max = 10) {
+        return Math.floor(Math.random() * max);
+    }
+
+    showTeam() {
+        console.log(`GRUPO A: ${this.teams[0].name}, ${this.teams[1].name}`)
+        console.log(`GRUPO A: ${this.teams[2].name}, ${this.teams[3].name}`)
+        console.log(`GRUPO A: ${this.teams[4].name}, ${this.teams[5].name}`)
+        console.log(`GRUPO A: ${this.teams[6].name}, ${this.teams[7].name}`)
+    }
+
+    quarterFinals() {
+        this.semiFinals = []
+        this.teams[0].goalsFor = this.generateGoals()
+        this.teams[1].goalsFor = this.generateGoals()
+        this.teams[2].goalsFor = this.generateGoals()
+        this.teams[3].goalsFor = this.generateGoals()
+        this.teams[4].goalsFor = this.generateGoals()
+        this.teams[5].goalsFor = this.generateGoals()
+        this.teams[6].goalsFor = this.generateGoals()
+        this.teams[7].goalsFor = this.generateGoals()
+        while (this.teams[0].goalsFor === this.teams[3].goalsFor){
+            this.teams[0].goalsFor = this.generateGoals()
+            this.teams[3].goalsFor = this.generateGoals()
+        }
+        if (this.teams[0].goalsFor>this.teams[3].goalsFor) {
+            this.semiFinals.push(this.teams[0])
+            console.log(`${this.teams[0].name} ${this.teams[0].goalsFor} - ${this.teams[3].name} ${this.teams[3].goalsFor} => ${this.teams[0].name}       (C1: Camp A - Subc B)`)
+        }else {
+            this.semiFinals.push(this.teams[3])
+            console.log(`${this.teams[0].name} ${this.teams[0].goalsFor} - ${this.teams[3].name} ${this.teams[3].goalsFor} => ${this.teams[3].name}       (C1: Camp A - Subc B)`)
+        }
+        while (this.teams[2].goalsFor === this.teams[1].goalsFor){
+            this.teams[2].goalsFor = this.generateGoals()
+            this.teams[1].goalsFor = this.generateGoals()
+        }
+        if (this.teams[2].goalsFor>this.teams[1].goalsFor) {
+            this.semiFinals.push(this.teams[2])
+            console.log(`${this.teams[2].name} ${this.teams[2].goalsFor} - ${this.teams[1].name} ${this.teams[1].goalsFor} => ${this.teams[2].name}       (C1: Camp B - Subc A)`)
+        }else {
+            this.semiFinals.push(this.teams[1])
+            console.log(`${this.teams[2].name} ${this.teams[2].goalsFor} - ${this.teams[1].name} ${this.teams[1].goalsFor} => ${this.teams[1].name}       (C1: Camp B - Subc A)`)
+        }
+        while (this.teams[4].goalsFor === this.teams[7].goalsFor){
+            this.teams[4].goalsFor = this.generateGoals()
+            this.teams[7].goalsFor = this.generateGoals()
+        }
+        if (this.teams[4].goalsFor>this.teams[7].goalsFor) {
+            this.semiFinals.push(this.teams[4])
+            console.log(`${this.teams[4].name} ${this.teams[4].goalsFor} - ${this.teams[7].name} ${this.teams[7].goalsFor} => ${this.teams[4].name}       (C1: Camp C - Subc D)`)
+        }else {
+            this.semiFinals.push(this.teams[7])
+            console.log(`${this.teams[4].name} ${this.teams[4].goalsFor} - ${this.teams[7].name} ${this.teams[7].goalsFor} => ${this.teams[7].name}       (C1: Camp C - Subc D)`)
+        }
+        while (this.teams[6].goalsFor === this.teams[5].goalsFor){
+            this.teams[6].goalsFor = this.generateGoals()
+            this.teams[5].goalsFor = this.generateGoals()
+        }
+        if (this.teams[6].goalsFor>this.teams[5].goalsFor) {
+            this.semiFinals.push(this.teams[6])
+            console.log(`${this.teams[6].name} ${this.teams[6].goalsFor} - ${this.teams[5].name} ${this.teams[5].goalsFor} => ${this.teams[6].name}       (C1: Camp C - Subc D)`)
+        }else {
+            this.semiFinals.push(this.teams[5])
+            console.log(`${this.teams[6].name} ${this.teams[6].goalsFor} - ${this.teams[5].name} ${this.teams[5].goalsFor} => ${this.teams[5].name}       (C1: Camp C - Subc D)`)
+        }
+    }
+
 }
--- index.js
+++ index.js
@@ -5,9 +5,17 @@ import Play_off from "./Play_off/Play_off.js"
 
 
 const teams = ["Austria", "England", "Germany", "Spain", "Sweden", "Russia", "France", "Iceland"]
-const finals = new Play_off ("Finals", teams)
+const finals = new Play_off ("finals", teams)
 
 
 console.log("=================================================")
 console.log("== COMIENZA LAS FASES ELIMINATORIAS DEL TORNEO ==")
 console.log("=================================================")
+
+console.log("Equipos que participan en el playoff:")
+
+finals.showTeam()
+
+console.log("======== CUARTOS DE FINAL ======")
+
+finals.quarterFinals()
